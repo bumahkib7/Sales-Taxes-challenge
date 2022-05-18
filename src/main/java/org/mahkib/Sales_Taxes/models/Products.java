ccb139604b23b93b1cd35c72fba3a38ace889b52
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-05-18 09:02:08 +0200

    Adding Constants

---
+++ src/main/java/org/mahkib/Sales_Taxes/models/Products.java
@@ -0,0 +1,49 @@
+package org.mahkib.Sales_Taxes.models;
+
+@ToString
+@Getter
+@Setter
+
+public class Products {
+
+
+
+	public static Products create(int quantity , String name, ProductType type, ProductOrigin origin, Money price) {
+		return new Products (quantity, name, type, origin, price);
+	}
+
+	private final String name;
+	private final ProductType type;
+	private final ProductOrigin origin;
+	private final Money price;
+	private final int quantity;
+
+	private Products(int Quantity , String name, ProductType type, ProductOrigin origin, Money price) {
+		this.name = name;
+		this.type = type;
+		this.origin = origin;
+		this.price = price;
+		this.quantity = Quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ProductType getType() {
+		return type;
+	}
+
+	public Money getPrice() {
+		return price;
+	}
+
+	public boolean isImported() {
+		return origin == ProductOrigin.IMPORTED;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+}
